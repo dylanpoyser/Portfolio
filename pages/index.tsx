62b3cedea5a3272cbafe80e934711dc79ec12224
Dylan Poyser <80566035+dylanpoyser@users.noreply.github.com>
2023-02-09 03:05:46 -0500

    added smooth scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
-    scrollbar-hidden scrollbar-track-gray-400/20 scrollbar-thumb-[#0a98f7]/80 scroll-smooth">
+    scrollbar-none scrollbar-track-gray-400/20 scrollbar-thumb-[#0a98f7]/80 scroll-smooth">
       <Head>
         <title>Dylan Poyser</title>
         <meta name="description" content="Dylan Poyser" />
